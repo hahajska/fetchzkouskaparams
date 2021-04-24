866a4138abee15a9f6d6d41fe7b90449c0abc114
TomasSchlogl <thomas537@seznam.cz>
2021-04-25 01:15:32 +0200

    zaklad

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
-import "./App.scss";
 import React, { useState, useEffect } from "react";
+import "./App.scss";
 
 import {
   BrowserRouter as Router,
@@ -9,12 +9,7 @@ import {
   useParams,
 } from "react-router-dom";
 
-import Support from "./Support";
-import Info from "./Info";
-import Home from "./Home";
-import Prices from "./Prices";
-
-function App() {
+function Home() {
   const [data, setData] = useState([]);
   const getData = () => {
     fetch("data.json", {
@@ -35,11 +30,9 @@ function App() {
   useEffect(() => {
     getData();
   }, []);
+
   return (
-    /*     <Router>
-      <Switch>
-        <Route path="/:id" component={Funguj} />
-      </Switch>
+    <div>
       {data &&
         data.length > 0 &&
         data.map((item, key) => (
@@ -52,38 +45,8 @@ function App() {
             </Link>
           </div>
         ))}
-    </Router> */
-    <Router>
-      <div className="navbarWrapper">
-        <li>
-          <Link to="/support">support</Link>
-        </li>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/info">Info</Link>
-        </li>
-        <li>
-          <Link to="/prices">prices</Link>
-        </li>
-      </div>
-      <Switch>
-        <Route path="/prices">
-          <Prices />
-        </Route>
-        <Route path="/info">
-          <Info />
-        </Route>
-        <Route path="/support">
-          <Support />
-        </Route>
-        <Route exact path="/">
-          <Home setData={setData} />
-        </Route>
-      </Switch>
-    </Router>
+    </div>
   );
 }
 
-export default App;
+export default Home;
---
+++ src/Prices.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Prices() {
+  return (
+    <div>
+      <h1>prices</h1>
+    </div>
+  );
+}
+
+export default Prices;
